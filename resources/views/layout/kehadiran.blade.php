af4bb27b1895d98223e0779038e44b2c2a5afa27
adindameisyaaa <144095598+adindameisyaaa@users.noreply.github.com>
2024-06-09 18:05:30 +0700

    (kehadiran)

--- resources/views/layout/kehadiran.blade.php
+++ resources/views/layout/kehadiran.blade.php
@@ -1,30 +1,45 @@
 @extends('app')
 @section('content')
-    <div class="container form-container">
-        <h2 class="text-center mb-4 text-4xl font-bold text-black">Kehadiran Karyawan</h2>
-        <form id="attendanceForm">
-            <div class="form-group">
-                <label for="shift">Shift :</label>
-                <Select type="Select" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    Pilih Shift
-                    <option selected>Pilih Shift Kehadiran</option>
-                    <option value="1">Siang</option>
-                    <option value="2">Malam</option>
-                </Select>
-            </div>
+<div class="m-12 p-6 rounded-md shadow-xl bg-[#fff]">
+    <h1 class="text-black text-4xl font-bold flex justify-center p-4 rounded-md">Kehadiran Karyawan</h1>
+    <form class="text-black" id="kehadiranForm">
+        <div class="form-group">
+            <table border="0">
+                <tr>
+                    <td> <label for="shift">Shift</label> </td>
+                    <td>:</td>
+                    <td> <Select type="Select" class="btn btn-primary dropdown-toggle w-36" data-bs-toggle="dropdown" aria-expanded="false">
+                            Pilih Shift
+                            <option selected>Pilih Shift</option>
+                            <option value="1">Siang</option>
+                            <option value="2">Malam</option>
+                        </Select> </td>
+                </tr>
+
+                <tr>
+                    <td> <label for="sesi">Sesi </label> </td>
+                    <td>:</td>
+                    <td><Select type="Select" class="btn btn-primary dropdown-toggle w-36" data-bs-toggle="dropdown" aria-expanded="false">
+                            Pilih Sesi
+                            <option selected>Pilih Sesi</option>
+                            <option value="1">Kedatangan</option>
+                            <option value="2">kepulangan</option>
+                        </Select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td><label for="date">Masukkan Tanggal :</label></td>
+                </tr>
+
+
+
+
+            </table>
+
 
             <div class="form-group">
-                <label for="sesi">Sesi :</label>
-                <Select type="Select" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    Pilih Sesi
-                    <option selected>Pilih Sesi</option>
-                    <option value="1">Kedatangan</option>
-                    <option value="2">kepulangan</option>
-                </Select>
-            </div>
 
-            <div class="form-group">
-                <label for="date">Masukkan Tanggal :</label>
                 <input type="date" class="form-control col-sm-2 col-form-label" id="date" name="date">
             </div>
 
@@ -64,54 +79,54 @@
                 <input type="text" class="form-control note mt-2" id="note3" placeholder="Keterangan">
             </div>
 
-            <button type="button" class="btn btn-primary btn-block" onclick="submitForm()">Submit</button>
-        </form>
+            <button class="btn btn-primary w-32 " type="button" onclick="submitForm()">Submit</button>
+    </form>
 
-        <div id="popup" class="modal fade" tabindex="-1" role="dialog">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Konfirmasi</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Data anda telah tersimpan.</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" onclick="confirmPopup(true)">Oke</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="confirmPopup(false)">Cancel</button>
-                    </div>
+    <div id="popup" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Konfirmasi</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Data anda akan disimpan. Lanjutkan?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" onclick="confirmPopup(true)">Oke</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="confirmPopup(false)">Cancel</button>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- Script start -->
-    <script>
-        function toggleNoteField(karyawanNumber) {
-            const status = document.getElementById(`status${karyawanNumber}`).value;
-            const noteField = document.getElementById(`note${karyawanNumber}`);
-            if (status === 'izin' || status === 'telat') {
-                noteField.style.display = 'block';
-            } else {
-                noteField.style.display = 'none';
-            }
+<!-- Script start -->
+<script>
+    function toggleNoteField(karyawanNumber) {
+        const status = document.getElementById(`status${karyawanNumber}`).value;
+        const noteField = document.getElementById(`note${karyawanNumber}`);
+        if (status === 'izin' || status === 'telat') {
+            noteField.style.display = 'block';
+        } else {
+            noteField.style.display = 'none';
         }
+    }
 
-        function submitForm() {
-            $('#popup').modal('show');
-        }
+    function submitForm() {
+        $('#popup').modal('show');
+    }
 
-        function confirmPopup(isConfirmed) {
-            if (isConfirmed) {
-                alert('Program akan kembali mengarah ke halaman utama.');
-                window.location.href = '/menu';
-            } else {
-                $('#popup').modal('hide');
-            }
+    function confirmPopup(isConfirmed) {
+        if (isConfirmed) {
+            alert('Program akan kembali mengarah ke halaman utama.');
+            window.location.href = '/menu';
+        } else {
+            $('#popup').modal('hide');
         }
-    </script>
-    <!-- Script end -->
+    }
+</script>
+<!-- Script end -->
 @endsection
